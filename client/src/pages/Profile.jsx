3ad51b750821337d9d6c1ad3f8c12432a3c8c4f1
Zach <itsaboy3366@gmail.com>
2024-03-22 02:14:07 -0400

    more stuff added to user page, fixed bug with refresh

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,10 +2,13 @@ import { useEffect, useContext } from "react";
 import ProfileNav from "../components/Profile/ProfileNav";
 import MyAccount from "../components/Profile/MyAccount";
 import { FinanceContext } from "../context/FinanceContext";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 export default function Profile() {
   const { setActivePage, setSECForm, setOptionStrategy } =
     useContext(FinanceContext);
+
+  const { user } = useAuthContext();
 
   useEffect(() => {
     setActivePage("profile");
