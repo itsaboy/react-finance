6b4395a8a7661a5ca9c0c3746891d9bf0d1761f0
Zach <itsaboy3366@gmail.com>
2024-06-11 05:57:53 -0400

    Inc changed to LLC in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="bg-transparent h-full">
       <div className="mx-auto max-w-7xl px-6 py-12 grid place-center">
         <p className="text-center text-xs md:text-sm leading-5 text-gray-900">
-          &copy; 2024 Zach of All Trades, Inc. All rights reserved.
+          &copy; 2024 Zach of All Trades, LLC. All rights reserved.
         </p>
       </div>
     </footer>
